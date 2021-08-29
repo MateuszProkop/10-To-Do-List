3000c7f48de207fb47b82a9119e1c8e15989a14d
Mateusz_Prokop <mateusz.prokopp@gmail.com>
2021-08-29 23:50:04 +0200

    menu

---
+++ To_do_list.py
@@ -0,0 +1,9 @@
+user_choice = -1
+while user_choice != 5:
+    print("1. Pokaż zadania")
+    print("2. Dodaj zadanie")
+    print("3. Usuń zadanie")
+    print("4. Zapisz zmiany do pliku")
+    print("5. Wyjdź")
+
+    user_choice = int(input("Wybierz liczbę: "))
